13ed9ad3b0996f99d84d1c2b8118c486381e1b3c
ferdianafsa <farisafza201002@gmail.com>
2020-03-11 10:19:01 +0700

    image gallery

--- app/src/main/java/com/example/stores/UI/Stores/AddStore.kt
+++ app/src/main/java/com/example/stores/UI/Stores/AddStore.kt
@@ -1,6 +1,10 @@
 package com.example.stores.UI.Stores
 
+import android.Manifest
 import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +13,7 @@ import com.example.myapplication.Services.ApiClient
 import com.example.myapplication.Services.StoreApi
 import com.example.stores.Model.Stores.Data
 import com.example.stores.R
+import com.github.dhaval2404.imagepicker.ImagePicker
 import kotlinx.android.synthetic.main.add_store.*
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MultipartBody
@@ -20,8 +25,12 @@ import java.io.File
 
 class AddStore :AppCompatActivity() {
 
-//    private val OPEN_MEDIA_PICKER = 1  // Request code
-
+//    companion object {
+//        //image pick code
+//        private val IMAGE_PICK_CODE = 1000;
+//        //Permission code
+//        private val PERMISSION_CODE = 1001;
+//    }
 //    val images: ArrayList<MultipartBody.Part> = ArrayList()
 var filePath:String? = String()
 
@@ -32,72 +41,46 @@ var filePath:String? = String()
 
         val apiInterface: StoreApi = ApiClient.getClient().create(StoreApi::class.java)
 
-//        btnPickLogo.setOnClickListener {
-//            ImagePicker.with(this)
-//                .crop(1f, 1f)               //Crop Square image(Optional)
-//                .compress(1024)         //Final image size will be less than 1 MB(Optional)
-//                .maxResultSize(
-//                    1080,
-//                    1080
-//                )  //Final image resolution will be less than 1080 x 1080(Optional)
-//                .start { resultCode, data ->
-//                    if (resultCode == Activity.RESULT_OK) {
-//                        //Image Uri will not be null for RESULT_OK
-//                        val fileUri = data?.data
-//                        ivLogoAdd.setImageURI(fileUri)
-//
-//                        //You can get File object from intent
-//                        val file: File? = ImagePicker.getFile(data)
-//
-//                        //You can also get File Path from intent
-//                        val filePath: String? = ImagePicker.getFilePath(data)
-//                    } else if (resultCode == ImagePicker.RESULT_ERROR) {
-//                        Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
-//                    } else {
-//                        Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
-//                    }
-//                }
+        btnPickCover.setOnClickListener {
+            //check runtime permission
+            ImagePicker.with(this)
+                .crop(1f, 1f)               //Crop Square image(Optional)
+                .compress(1024)         //Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)  //Final image resolution will be less than 1080 x 1080(Optional)
+                .start { resultCode, data ->
+                    if (resultCode == Activity.RESULT_OK) {
+                        //Image Uri will not be null for RESULT_OK
+                        val fileUri = data?.data
+                        ivCoverAdd.setImageURI(fileUri)
 
-//            if(EasyPermissions.hasPermissions(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)){
-//                val intent = Intent(this, Gallery::class.java)
-////                // Set the title for toolbar
-//                intent.putExtra("title", "Select media")
-////
-////                // Mode 1 for both images and videos selection, 2 for images only and 3 for videos!
-////                intent.putExtra("mode", 2)
-////                intent.putExtra("maxSelection", 5) // Optional
-////                startActivityForResult(intent, OPEN_MEDIA_PICKER)
-//            }
-//            else {
-//                // tampilkan permission request saat belum mendapat permission dari user
-//                EasyPermissions.requestPermissions(this,"This application need your permission to access photo gallery.",991,android.Manifest.permission.READ_EXTERNAL_STORAGE)
-//            }
-//        }
-//        btnPickCover.setOnClickListener {
-//            ImagePicker.with(this)
-//                .crop(1f, 1f)               //Crop Square image(Optional)
-//                .compress(1024)         //Final image size will be less than 1 MB(Optional)
-//                .maxResultSize(1080, 1080)  //Final image resolution will be less than 1080 x 1080(Optional)
-//                .start { resultCode, data ->
-//                    if (resultCode == Activity.RESULT_OK) {
-//                        //Image Uri will not be null for RESULT_OK
-//                        val fileUri = data?.data
-//                        ivCoverAdd.setImageURI(fileUri)
-//
-//                        //You can get File object from intent
-//                        val file:File? = ImagePicker.getFile(data)
-//
-//                        //You can also get File Path from intent
-//                        val filePath:String? = ImagePicker.getFilePath(data)
-//                    } else if (resultCode == ImagePicker.RESULT_ERROR) {
-//                        Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
-//                    } else {
-//                        Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
-//                    }
-//                }
-//    }
+                        //You can get File object from intent
+                    } else if (resultCode == ImagePicker.RESULT_ERROR) {
+                        Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
+                    }
+                }
 
+        }
+        btnPickLogo.setOnClickListener {
+            //check runtime permission
+            ImagePicker.with(this)
+                .crop(1f, 1f)               //Crop Square image(Optional)
+                .compress(1024)         //Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)  //Final image resolution will be less than 1080 x 1080(Optional)
+                .start { resultCode, data ->
+                    if (resultCode == Activity.RESULT_OK) {
+                        //Image Uri will not be null for RESULT_OK
+                        val fileUri = data?.data
+                        ivLogoAdd.setImageURI(fileUri)
 
+                        //You can get File object from intent
+                    } else if (resultCode == ImagePicker.RESULT_ERROR) {
+                        Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
+                    }
+                }
 
         form {
             input(etstorename) {
@@ -132,6 +115,7 @@ var filePath:String? = String()
                 val storeState = etstorestate?.text.toString()
                 val storeCountry = etstorecountry.text.toString()
                 val storePhone = etstorephone.text.toString()
+            //check runtime permission
 
 
                     mediaType = "text/plain".toMediaType()
@@ -171,43 +155,37 @@ var filePath:String? = String()
         }
 
     }
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//        // Check which request we're responding to
-//        if (requestCode == OPEN_MEDIA_PICKER) {
-//            // Make sure the request was successful
-//            if (resultCode == Activity.RESULT_OK && data != null) {
-//                val selectionResult = data.getStringArrayListExtra("result")
-//                selectionResult.forEach {
-//                    try {
-//                        Log.d("MyApp", "Image Path : " + it)
-//                        val uriFromPath = Uri.fromFile(File(it))
-//                        Log.d("MyApp", "Image URI : " + uriFromPath)
-//                        // Convert URI to Bitmap
-//                        val bm = BitmapFactory.decodeStream(
-//                            contentResolver.openInputStream(uriFromPath))
+    }
+//    private fun pickImageFromGallery() {
+//        //Intent to pick image
+//        val intent = Intent(Intent.ACTION_PICK)
+//        intent.type = "image/*"
+//        startActivityForResult(intent, IMAGE_PICK_CODE)
+//    }
 //
-//                        val file = File(it)
-//                        var mediaType = "image/png".toMediaType()
-//                        val fileReqBody = RequestBody.create(mediaType, file)
-//                        val filePart: MultipartBody.Part = MultipartBody.Part.createFormData("product[images][]", file.name, fileReqBody)
-//
-//                        images.add(filePart)
-//
-//                        val imageView = ImageView(this)
-//                        imageView.layoutParams = LinearLayout.LayoutParams(250, 250) // value is in pixels
-//
-//                        imageView.setImageBitmap(bm)
-//
-//                        // Add ImageView to LinearLayout
-//                        LlAdd?.addView(imageView)
-//
-//                    } catch
-//                        (e: FileNotFoundException) {
-//                        e.printStackTrace()
-//                    }
+//    //handle requested permission result
+//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+//        when(requestCode){
+//            PERMISSION_CODE -> {
+//                if (grantResults.size >0 && grantResults[0] ==
+//                    PackageManager.PERMISSION_GRANTED){
+//                    //permission from popup granted
+//                    pickImageFromGallery()
+//                }
+//                else{
+//                    //permission from popup denied
+//                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
 //                }
 //            }
 //        }
 //    }
+//
+//    //handle result of picked image
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+//            ivCoverAdd.setImageURI(data?.data)
+//            }
+//    }
+
 }
